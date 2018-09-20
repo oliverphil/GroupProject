c61b5e07aab46c7d7f873d0d7f10a6db48a0c294
Dylan <Dylan@DESKTOP-JIO9T4I>
2018-09-20 17:45:38 +1200

    "Completed preliminary Tile Object"

--- src/gameWorld/Tile.java
+++ src/gameWorld/Tile.java
@@ -1,5 +1,94 @@
 package gameWorld;
 
-public class Tile {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * A Tile object is a single Tile making up the board. It may contain and 
+ * item or anywhere from 0 to 4 walls.
+ * @author Dylan
+ *
+ */
+public class Tile {
+	private List<String> walls;
+	private Item item;
+	
+	/**
+	 * Constructs a Tile object with no walls.
+	 */
+	public Tile() {
+		this.walls = new ArrayList<String>();
+		this.item = null;
+	}
+	
+	/**
+	 * Constructs a Tile object using a list of Strings to indicate walls.
+	 * @param walls
+	 */
+	public Tile(List<String> walls) {
+		this.walls.addAll(walls);
+	}
+	
+	/**
+	 * @return whether the Tile contains a wall to the North
+	 */
+	public boolean hasWallNorth() {
+		return walls.contains("north");
+	}
+	
+	/**
+	 * @return whether the Tile contains a wall to the South
+	 */
+	public boolean hasWallSouth() {
+		return walls.contains("south");
+	}
+	
+	/**
+	 * @return whether the Tile contains a wall to the East
+	 */
+	public boolean hasWallEast() {
+		return walls.contains("east");
+	}
+	
+	/**
+	 * @return whether the Tile contains a wall to the West
+	 */
+	public boolean hasWallWest() {
+		return walls.contains("west");
+	}
+	
+	/**
+	 * Adds a wall to a tile if it is not already added
+	 * @param wall
+	 * @return whether the wall was successfully added
+	 */
+	public boolean addWall(String wall) {
+		if (this.walls.contains(wall)) return false;
+		
+		this.walls.add(wall);
+		
+		return true;
+	}
+
+	/**
+	 * @return the item
+	 */
+	public Item getItem() {
+		return item;
+	}
+
+	/**
+	 * @param item the item to set
+	 */
+	public void setItem(Item item) {
+		if (!hasItem()) this.item = item;
+	}
+	
+	/**
+	 * Returns whether the tile has an item.
+	 * @return
+	 */
+	public boolean hasItem() {
+		return item!=null;
+	}
 }
