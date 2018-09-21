db8d3feaa998c876fcb3aad800a1fa160382af54
Dylan <Dylan@DESKTOP-JIO9T4I>
2018-09-21 15:46:14 +1200

    Player class now holds current direction and turning methods

--- src/gameWorld/Player.java
+++ src/gameWorld/Player.java
@@ -10,10 +10,12 @@ import java.util.List;
  */
 public class Player extends Character {
 	private List<Item> bag;
+	private String direction;
 
 	public Player (String name) {
 		super(name);
 
+		this.setDirection("north");
 		bag = new ArrayList<Item>();
 	}
 
@@ -29,5 +31,59 @@ public class Player extends Character {
 	 */
 	public void setBag(List<Item> bag) {
 		this.bag = bag;
+	}
+
+	/**
+	 * Changes the direction clockwise from where the customer is facing.
+	 */
+	public void turnRight() {
+		switch (getDirection()) {
+			case "north" : 
+				setDirection("east");
+				break;
+			case "east" : 
+				setDirection("south");
+				break;
+			case "south" : 
+				setDirection("west");
+				break;
+			case "west" : 
+				setDirection("north");
+				break;
+		}
+	}
+	
+	/**
+	 * Changes the direction counter clockwise from where the customer is facing.
+	 */
+	public void turnLeft() {
+		switch (getDirection()) {
+			case "north" : 
+				setDirection("west");
+				break;
+			case "west" : 
+				setDirection("south");
+				break;
+			case "south" : 
+				setDirection("east");
+				break;
+			case "east" : 
+				setDirection("north");
+				break;
+		}
+	}
+	
+	/**
+	 * @return the direction
+	 */
+	public String getDirection() {
+		return direction;
+	}
+
+	/**
+	 * @param direction the direction to set
+	 */
+	public void setDirection(String direction) {
+		this.direction = direction;
 	}
 }
