355e812491a8f45175dbb8c3160b6ef5a67f17d8
Lithowan <wanja.leuthold@gmail.com>
2018-10-01 13:18:32 +1300

    added two JAXB annotations to GameWorld

--- src/gameWorld/GameWorld.java
+++ src/gameWorld/GameWorld.java
@@ -2,11 +2,12 @@ package gameWorld;
 
 import java.util.Observable;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
 
 /**
  * GameWorld class is the API for the game.
+ * 
  * @author Dylan
  *
  */
@@ -14,31 +15,34 @@ import javax.xml.bind.annotation.XmlType;
 @XmlRootElement
 public class GameWorld extends Observable {
 
-	Player player;
-	Board board;
-	
-	/**
-	 * Constructs the GameWorld object, allowing you to play the game.
-	 */
-	public GameWorld() {
-		player = new Player("Player");
-		board = new Board();
-		player.setView(new ViewDescriptor(player, board));
-	}
-	
-	/**
-	 * @return the current ViewDescriptor of the player
-	 */
-	public ViewDescriptor getViewDescriptor() {
-		return player.getView();
-	}
-	
-	/**
-	 * Updates the renderer.
-	 */
-	public void update() {
-		setChanged();
-		notifyObservers(getViewDescriptor());
-	}
-	//whenever a view changes use update();
+  @XmlElement
+  Player player;
+  @XmlElement
+  Board board;
+
+  /**
+   * Constructs the GameWorld object, allowing you to play the game.
+   */
+  public GameWorld() {
+    player = new Player("Player");
+    board = new Board();
+    player.setView(new ViewDescriptor(player, board));
+  }
+
+  /**
+   * Returns the current ViewDescriptor.
+   * @return the current ViewDescriptor of the player
+   */
+  public ViewDescriptor getViewDescriptor() {
+    return player.getView();
+  }
+
+  /**
+   * Updates the renderer.
+   */
+  public void update() {
+    setChanged();
+    notifyObservers(getViewDescriptor());
+  }
+  // whenever a view changes use update();
 }
