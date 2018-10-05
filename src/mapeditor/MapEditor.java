1651db8ed516d1f8c2edd39c4c932fee9573d7e4
Philip Oliver <oliverphil@ecs.vuw.ac.nz>
2018-10-05 13:42:57 +1300

    Fixed style in Persistence package

--- src/mapeditor/MapEditor.java
+++ src/mapeditor/MapEditor.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
