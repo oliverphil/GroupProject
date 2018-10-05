f66a650db18d8c5e0fb95ca3bef21632395c1f2e
Philip Oliver <oliverphil@ecs.vuw.ac.nz>
2018-10-05 13:29:38 +1300

    Fixed style issues in Notification
    Javadocs still need to be added

--- src/application/Notification.java
+++ src/application/Notification.java
@@ -1,53 +1,55 @@
 package application;
-import javafx.stage.*;
-import javafx.scene.*;
-import javafx.scene.layout.*;
-import javafx.scene.control.*;
-import javafx.geometry.*;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
- * Previously discussed the idea of having information "pop-up";
- * This class would create alerts/notifications
+ * Previously discussed the idea of having information "pop-up"; This class would create
+ * alerts/notifications.
  */
 
 public class Notification {
 
-	private static Button closeButton = new Button("Exit window");
-	private String windowTitle;
-	private String displayMessage;
-	
-	public Notification(String window, String message, String closeButton){
-		this.windowTitle = window;
-		this.displayMessage = message;
-		changeCloseButtonContent(closeButton);
-		display(windowTitle, displayMessage);
-	}
-	
-    public static void display(String title, String message) {
-        Stage window = new Stage();
+  private static Button closeButton = new Button("Exit window");
+  private String windowTitle;
+  private String displayMessage;
 
-        //Block events to other windows
-        window.initModality(Modality.APPLICATION_MODAL);
-        window.setTitle(title);
-        window.setMinWidth(250);
+  public Notification(String window, String message, String closeButton) {
+    this.windowTitle = window;
+    this.displayMessage = message;
+    changeCloseButtonContent(closeButton);
+    display(windowTitle, displayMessage);
+  }
 
-        Label label = new Label();
-        label.setText(message);
-        closeButton.setOnAction(e -> window.close());
+  public static void display(String title, String message) {
+    Stage window = new Stage();
 
-        VBox layout = new VBox(50);
-        layout.getChildren().addAll(label, closeButton);
-        layout.setAlignment(Pos.CENTER);
+    // Block events to other windows
+    window.initModality(Modality.APPLICATION_MODAL);
+    window.setTitle(title);
+    window.setMinWidth(250);
 
-        //Display window and wait for it to be closed before returning
-        Scene scene = new Scene(layout);
-        window.setScene(scene);
-        window.showAndWait();
-    }
-    
-    public void changeCloseButtonContent(String newContent) {
-    	closeButton.setText(newContent);
-    }
+    Label label = new Label();
+    label.setText(message);
+    closeButton.setOnAction(e -> window.close());
+
+    VBox layout = new VBox(50);
+    layout.getChildren().addAll(label, closeButton);
+    layout.setAlignment(Pos.CENTER);
+
+    // Display window and wait for it to be closed before returning
+    Scene scene = new Scene(layout);
+    window.setScene(scene);
+    window.showAndWait();
+  }
+
+  public void changeCloseButtonContent(String newContent) {
+    closeButton.setText(newContent);
+  }
 
 }
