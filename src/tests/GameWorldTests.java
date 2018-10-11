451bc48f00438e2b01e24643387fdf76301e9576
ewensdyla <ewensdyla@enigma-cafe.ecs.vuw.ac.nz>
2018-10-12 10:53:23 +1300

    Started writing tests

--- src/tests/GameWorldTests.java
+++ src/tests/GameWorldTests.java
@@ -1,22 +1,106 @@
 package tests;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+import gameworld.Board;
 import gameworld.GameWorld;
 import gameworld.Player;
+import renderer.Renderer;
+import renderer.Renderer.ItemOnScreen;
 
 public class GameWorldTests {
 
-  private GameWorld game;
-  private Player player;
+  @Test
+  public void testStartPos_1() {
+    GameWorld game = new GameWorld();
+    Player player = game.getPlayer();
 
-  /**
-   * A method to run before each test. Creates a new renderer.
-   */
-  @BeforeEach
-  public void getNewGameWorld() {
-    game = new GameWorld();
-    player = new Player();
+    assertEquals(player.getLocation().getX(), 7);
+    assertEquals(player.getLocation().getY(), 7);
   }
 
+  @Test
+  public void testValidMovement_1() {
+    GameWorld game = new GameWorld();
+    Player player = game.getPlayer();
+
+    //movement commands
+    game.openDoor();
+    game.moveForward();
+
+    assertEquals(player.getLocation().getX(), 7);
+    assertEquals(player.getLocation().getY(), 4);
+  }
+
+  @Test
+  public void testValidMovement_2() {
+    GameWorld game = new GameWorld();
+    Player player = game.getPlayer();
+
+    //movement command
+    game.rotateRight();
+    assertEquals(player.getDirection(), "east");
+
+    //movement command
+    game.rotateRight();
+    assertEquals(player.getDirection(), "south");
+
+    //movement command
+    game.rotateRight();
+    assertEquals(player.getDirection(), "west");
+
+    //movement command
+    game.rotateRight();
+    assertEquals(player.getDirection(), "north");
+  }
+
+  @Test
+  public void testValidMovement_3() {
+    GameWorld game = new GameWorld();
+    Player player = game.getPlayer();
+
+    //movement command
+    game.rotateLeft();
+    assertEquals(player.getDirection(), "west");
+
+    //movement command
+    game.rotateLeft();
+    assertEquals(player.getDirection(), "south");
+
+    //movement command
+    game.rotateLeft();
+    assertEquals(player.getDirection(), "east");
+
+    //movement command
+    game.rotateLeft();
+    assertEquals(player.getDirection(), "north");
+  }
+
+  @Test
+  public void testValidMovement_4() {
+    GameWorld game = new GameWorld();
+
+    //movement command
+    game.openDoor();
+    game.rotateLeft();
+    game.rotateLeft();
+    game.moveBackwards();
+
+    Player player = game.getPlayer();
+    assertEquals(player.getLocation().getX(), 7);
+    assertEquals(player.getLocation().getY(), 4);
+  }
+
+  @Test
+  public void testInteract_1() {
+    GameWorld game = new GameWorld();
+
+    //ItemOnScreen item = new ItemOnScreen(0,0,0,0,0, "flask");
+
+    Player player = game.getPlayer();
+    assertEquals(player.getLocation().getX(), 7);
+    assertEquals(player.getLocation().getY(), 4);
+  }
 }
