94c61d63acc067d9271ba4d248d35950089e8b77
Philip Oliver <oliverphil@ecs.vuw.ac.nz>
2018-10-14 13:24:00 +1300

    Wrote example test to run MapEditor

--- src/tests/MapEditorTests.java
+++ src/tests/MapEditorTests.java
@@ -1,7 +1,48 @@
 package tests;
 
+import static org.junit.Assert.assertTrue;
+
+import javafx.application.Application;
+
+import mapeditor.MapEditor;
+
+import org.junit.Test;
+
 public class MapEditorTests {
 
-  //TODO: Tests belong here 
-  
+  private volatile boolean success = false;
+
+  @Test
+  public void setup() {
+    Thread thread = new Thread() {
+      @Override
+      public void run() {
+        try {
+          Application.launch(MapEditor.class);
+          success = true;
+        } catch (Throwable t) {
+          if (t.getCause() != null && t.getCause().getClass().equals(InterruptedException.class)) {
+            success = true;
+            return;
+          }
+        }
+      }
+    };
+    thread.setDaemon(true);
+    thread.start();
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      // do nothing, does not affect running
+    }
+    thread.interrupt();
+    try {
+      thread.join(1);
+    } catch (InterruptedException e) {
+      // do nothing, does not affect running
+    }
+
+    assertTrue(success);
+  }
+
 }
