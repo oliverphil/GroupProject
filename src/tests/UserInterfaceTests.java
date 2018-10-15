df1cdde5444299d12f31fbdbeb8172b8cc6b0baf
Maria DaRocha <mariao.darocha@gmail.com>
2018-10-16 01:00:39 +1300

    Update src/tests/UserInterfaceTests.java

--- src/tests/UserInterfaceTests.java
+++ src/tests/UserInterfaceTests.java
@@ -2,33 +2,52 @@ package tests;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 import application.UserInterface;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.stage.Stage;
 import mapeditor.MapEditor;
 
 public class UserInterfaceTests {
  
   private volatile boolean success = false;
   
+  @Before
+  public void setup() {
+    new JFXPanel();
+  }
+  
   @Test
-  public void userInterfaceLaunchTest() {
-    Thread thread = new Thread() {
+  public void createUserInterface() {
+    UserInterface ui = new UserInterface();
+    Thread thread = new Thread(new Runnable() {
+
       @Override
       public void run() {
-        try {
-          Application.launch(UserInterface.class);
-          success = true;
-        } catch (Throwable t) {
-          if (t.getCause() != null && t.getCause().getClass().equals(InterruptedException.class)) {
-            success = true;
-            return;
+        new JFXPanel(); // Initializes the JavaFx Platform
+        Platform.runLater(new Runnable() {
+
+          @Override
+          public void run() {
+            try {
+              ui.start(new Stage());
+              success = true;
+            } catch (Throwable t) {
+              if (t.getCause() != null
+                  && t.getCause().getClass().equals(InterruptedException.class)) {
+                success = true;
+                return;
+              }
+            }
           }
-        }
+        });
       }
-    };
-    thread.setDaemon(true);
+    });
     thread.start();
+
     try {
       Thread.sleep(3000);
     } catch (InterruptedException e) {
@@ -43,5 +62,4 @@ public class UserInterfaceTests {
 
     assertTrue(success);
   }
-
-}
+ }
